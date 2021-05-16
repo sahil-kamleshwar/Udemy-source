c0bc64910d12a8829bb1cfab81e3924539df10da
sahil-kamleshwar <77151429+sahil-kamleshwar@users.noreply.github.com>
2021-05-17 02:05:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// variables- most basic building blocks
+// variables- stores, access, modify===value
+// variablez- declare, assigns, operator, assign value
+
+let firstName="sahil kamleshwar";
+console.log(firstName);
---
+++ variables.js
@@ -0,0 +1,21 @@
+// assigning values later to the variable
+
+/* let name="sahil kamleshwar";
+let address, zip, state;
+address="b-49";
+zip="110018";
+state="New-delhi";
+console.log(address, zip, state);
+*/
+
+//let vs var vs const
+
+//using let
+let value="some value";
+//using var
+var name="sahil";
+//using const
+const lastName="kamleshwar";
+console.log(value);
+console.log(name);
+console.log(lastName);
